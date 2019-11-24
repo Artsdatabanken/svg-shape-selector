9a84c41aa17a835e9f2cbc303c8f3b9ad8ecc5b7
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-11-24 19:22:16 +0100

    Legend layout

--- src/Legend.js
+++ src/Legend.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-const Legend = ({ categories, size = 40 }) => (
-  <g>
+const Legend = ({ categories, x = 0, y = 20, size = 40 }) => (
+  <svg x={x} y={y}>
     {Object.keys(categories).map(key => {
       const e = categories[key];
       return (
@@ -15,10 +15,17 @@ const Legend = ({ categories, size = 40 }) => (
         />
       );
     })}
-  </g>
+  </svg>
 );
 
-const LegendItem = ({ title, fill, x, y, size }) => (
+const LegendItem = ({
+  title,
+  tooltip = "sadf jaldfks jj",
+  fill,
+  x,
+  y,
+  size
+}) => (
   <svg x={x} y={y}>
     <rect
       width={size}
@@ -28,6 +35,7 @@ const LegendItem = ({ title, fill, x, y, size }) => (
     <text fontSize={32} x={1.5 * size} y={32}>
       {title}
     </text>
+    <title>{tooltip}</title>
   </svg>
 );
 
