8d54c1972042ad48b6127a332b1a7d99e4d0b648
Bjørn <{ID}+{username}@users.noreply.github.com>
2020-02-07 13:31:28 +0100

    Variable size legend item

--- src/components/Legend.js
+++ src/components/Legend.js
@@ -1,4 +1,5 @@
 import React from "react";
+import LegendItem from './LegendItem'
 
 const Legend = ({ categories, x = 0, y = 20, size = 40 }) => (
   <svg x={x} y={y}>
@@ -19,23 +20,5 @@ const Legend = ({ categories, x = 0, y = 20, size = 40 }) => (
   </svg>
 );
 
-const LegendItem = ({ title, tooltip, fill, x, y, size }) => (
-  <svg x={x} y={y}>
-    <rect
-      width={size}
-      height={size}
-      style={{
-        fill: fill,
-        stroke: "rgb(0,0,0)",
-        strokeWidth: 1,
-        filter: "url(#f1)"
-      }}
-    ></rect>
-    <text fontSize={32} x={1.5 * size} y={32}>
-      {title}
-    </text>
-    <title>{tooltip}</title>
-  </svg>
-);
 
 export default Legend;
